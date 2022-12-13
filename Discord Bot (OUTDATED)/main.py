75affa8ac11e104f77d4b534f5d1949a6ce511fb
Nyle Nasir <61722426+nyle-cmd@users.noreply.github.com>
2022-12-12 21:37:41 -0500

    Update main.py

--- Discord Bot (OUTDATED)/main.py
+++ Discord Bot (OUTDATED)/main.py
@@ -1,14 +1,4 @@
-import random
+import bot
 
-def handle_response(message) -> str:
-        p_message = message.lower() #will automatically lower case user input
-
-        if p_message == 'hello': #bot will reply to any of these statements
-            return 'Whats up schmuck! '
-        if p_message == 'slap connor':
-            return '*slaps connor*'
-        if p_message == 'roll':
-            return str(random.randint(1,20))
-
-        if p_message== '!help': #when user inputs !help
-            return 'no help only pain'
+if __name__ == '__main__': #main method that will run discord bot
+    bot.run_discord_bot()
