d4594ad0a186692c4f7a839208858fca0ef4b7ff
Nyle Nasir <61722426+nyle-cmd@users.noreply.github.com>
2023-11-30 23:27:30 -0500

    Update responses.py

--- Discord Bot (OUTDATED)/responses.py
+++ Discord Bot (OUTDATED)/responses.py
@@ -4,11 +4,11 @@ def handle_response(message) -> str:
         p_message = message.lower() #will automatically lower case user input
 
         if p_message == 'hello': #bot will reply to any of these statements
-            return 'Whats up schmuck! '
+            return 'Whats up! '
         if p_message == 'Give me a high five!':
-            return '*No*'
+            return '*high five*'
         if p_message == 'roll':
             return str(random.randint(1,20))
 
         if p_message== '!help': #when user inputs !help
-            return 'no help only pain'
+            return 'What can I do?'
